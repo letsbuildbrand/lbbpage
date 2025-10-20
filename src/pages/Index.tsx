5d9dd57b2b61aa20dc84177910f56dee5289256c
[dyad] <propertyshodh2025@gmail.com>
2025-10-20 15:16:33 +0530

    [dyad] Added ModelViewer component - wrote 4 file(s), added @react-three/fiber, @react-three/drei, three package(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,6 +7,7 @@ import TestimonialsSection from "@/components/TestimonialsSection";
 import ContactSection from "@/components/ContactSection";
 import SplashCursor from "@/components/SplashCursor";
 import Footer from "@/components/Footer";
+import ModelViewer from "@/components/ModelViewer"; // Import ModelViewer
 
 const Index = () => {
   const navItems = [
@@ -43,6 +44,19 @@ const Index = () => {
       {/* Main Content Sections */}
       <main className="relative z-10 flex-grow pt-24"> {/* Added pt-24 to account for fixed navbar height and top spacing */}
         <HeroSection />
+        {/* Example ModelViewer usage */}
+        <div className="flex justify-center items-center py-16">
+          <ModelViewer
+            url="https://assets.science.nasa.gov/content/dam/science/psd/solar/2023/09/e/Earth_1_12756.glb?emrc=68f60299b5bc0"
+            width={400}
+            height={400}
+            autoRotate={true}
+            autoRotateSpeed={0.5}
+            enableManualRotation={true}
+            enableManualZoom={true}
+            environmentPreset="studio"
+          />
+        </div>
         <ServicesSection />
         <FeaturesSection />
         <AboutSection />
